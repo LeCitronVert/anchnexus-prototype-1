9e261a1e5a6a8a8841ff2c9c990c65dd827661fd
LeCitronVert <youcefphnixlord@gmail.com>
2024-08-14 17:58:27 +0200

    Le DASH

--- player.cs
+++ player.cs
@@ -17,6 +17,7 @@ public partial class player : CharacterBody2D
 	[Export]
 	public int maximumJumpHoldingFrames = 5;
 	public int currentJumpHoldingFrames = 0;
+	public float lastInputDirection = 1;
 
 
 
@@ -38,15 +39,17 @@ public partial class player : CharacterBody2D
 	[Export]
 	public bool groundedDashOnly = false;
 	[Export]
-	public int dashStrength = 500;
+	public int dashStrength = 900;
+	[Export]
+	public int dashLength = 7;
+	public int remainingDashLength = 0;
+	public Vector2 currentDashDirection;
 
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-
-		// todo : Rajouter le saut
 		bool isJumping = Input.IsActionJustPressed("ui_jump");
 		bool isDashing = Input.IsActionJustPressed("ui_dash");
 		bool isHoldingJump = Input.IsActionPressed("ui_jump");
@@ -54,10 +57,13 @@ public partial class player : CharacterBody2D
 
 		if (IsOnFloor() && velocity.Y == 0) {
 			currentJumpHoldingFrames = 0;
+			remainingDashLength = 0;
 		}
 
-
-		if (isJumping || isDashing) {
+		if (0 < remainingDashLength) {
+			remainingDashLength--;
+			velocity = DoDash(velocity, true);
+		} else if (isJumping || isDashing) {
 			velocity.Y = 0;
 
 			// Jump
@@ -142,9 +148,14 @@ public partial class player : CharacterBody2D
 	[Signal]
 	public delegate void UpdateDashUiEventHandler();
 
-	public Vector2 DoDash(Vector2 velocity) {
+	public Vector2 DoDash(Vector2 velocity, bool isHoldingDash = false) {
 		// todo : Ajouter un knockback en cas de collision avec un ennemi après le dash, sans dégâts subis
 		ConsumeDash();
+		if (!isHoldingDash) {
+			remainingDashLength = dashLength;
+		} else {
+			return currentDashDirection;
+		}
 
 		if (hasFullDash) {
 			return DoFullDash(velocity);
@@ -164,17 +175,62 @@ public partial class player : CharacterBody2D
 	private Vector2 DoFullDash(Vector2 velocity) {
 		GD.Print("dash : complet");	
 
+		velocity = AddHorizontalDashPower(velocity);
+		velocity = AddVerticalDashPower(velocity);
+
+		currentDashDirection = velocity;
+
 		return velocity;
 	}
 
 	private Vector2 DoHorizontalDash(Vector2 velocity) {
 		GD.Print("dash : horizontal");	
 
+		velocity.Y = 0;
+		velocity = AddHorizontalDashPower(velocity);
+
+		currentDashDirection = velocity;
+
 		return velocity;
 	}
 
 	private Vector2 DoVerticalDash(Vector2 velocity) {
 		GD.Print("dash : vertical");	
+
+		velocity.X = 0;
+		velocity = AddVerticalDashPower(velocity);
+
+		currentDashDirection = velocity;
+
+		return velocity;
+	}
+
+	private Vector2 AddHorizontalDashPower(Vector2 velocity) {
+		float direction = Input.GetAxis("ui_left", "ui_right");
+
+		// Direction par défaut (basée sur la dernière direction, puis sinon vers la droite)
+		if (0 == direction) {
+			direction = lastInputDirection;
+
+			if (0 == direction) {
+				direction = 1;
+			}
+		}
+
+		velocity.X = direction * dashStrength;
+
+		return velocity;
+	}
+
+	private Vector2 AddVerticalDashPower(Vector2 velocity) {
+		float direction = Input.GetAxis("ui_up", "ui_down");
+
+		// Direction par défaut (vers le haut uniquement si en dash vertical seulement)
+		if (0 == direction && hasVerticalDashOnly) {
+			direction = -1;
+		}
+
+		velocity.Y = direction * dashStrength;
 
 		return velocity;
 	}
@@ -197,6 +253,10 @@ public partial class player : CharacterBody2D
 		// Directions
 		float direction = Input.GetAxis("ui_left", "ui_right");
 		velocity.X = direction * speed;
+
+		if (0 != direction) {
+			lastInputDirection = direction;
+		}
 
 		return velocity;
 	}
